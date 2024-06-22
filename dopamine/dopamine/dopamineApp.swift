498509cef7ea401ebbd38a594829855c3960b4b5
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-06-22 19:15:36 +0900

    init

---
+++ dopamine/dopamine/dopamineApp.swift
@@ -0,0 +1,17 @@
+//
+//  dopamineApp.swift
+//  dopamine
+//
+//  Created by 최시훈 on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct dopamineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
